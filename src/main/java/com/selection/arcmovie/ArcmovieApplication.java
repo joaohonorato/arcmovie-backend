b1c325213a3d33d621c289655e15dc404c5a3aff
Assis <joaohonorato@gmail.com>
2020-03-09 20:38:23 -0300

    Initial commit

---
+++ src/main/java/com/selection/arcmovie/ArcmovieApplication.java
@@ -0,0 +1,13 @@
+package com.selection.arcmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArcmovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/selection/arcmovie/ArcmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selection.arcmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
